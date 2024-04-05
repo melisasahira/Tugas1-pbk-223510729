8d57db91d1ed6a9300f1bf8b2ddcf0188bb32c30
melisasahira <melisasahira@student.uir.ac.id>
2024-04-05 20:11:32 +0700

    first-commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Melisa Sahira
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener"></a>
+      <a href="https://vuejs.org/" target="_blank" rel="noopener"></a>
+    </h3>
+    <h4>
+      Dumai, 30 Desember 2003
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener"></a>
+      <a href="https://vuejs.org/" target="_blank" rel="noopener"></a>
+    </h4>
+  </div>
+</template>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    background-color: rgb(155, 213, 236);
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,81 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Pengalaman kerja</template>
+
+    Web Developer i'm a passionate developer in HTML,CSS,and JavaScript
+    <a href="https://vuejs.org/" target="_blank" rel="noopener"></a>
+    provides you with all information you need to get started.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Kemampuan</template>
+
+    
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener"></a>* HTML, CSS, javaScript
+    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener"></a>* Node.js, Express.js
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener"></a>
+    <a href="https://www.cypress.io/" target="_blank" rel="noopener"></a>*React, Vue.js
+    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
+      ></a
+    >
+
+    <br />
+
+     <code></code>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Pendidikan</template>
+
+    Universitas Islam Riau
+    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://router.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener"></a> Teknik Informatika (2022)
+    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener"></a>
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Kontak</template>
+
+    Email:melisasahira30@gmail.com
+    <a href="https://chat.vuejs.org" target="_blank" rel="noopener"></a>
+    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
+      ></a
+    >
+    <a href="https://news.vuejs.org" target="_blank" rel="noopener"></a> 
+    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"></a>
+    Telepon: +628 13 3456 890
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Skills</template>
+
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener"></a> HTML,CSS,JavaScript
+  </WelcomeItem>
+</template>
